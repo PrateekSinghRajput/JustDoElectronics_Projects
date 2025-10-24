5bedd323b96d71d47a8c4d032ac3f40738cfe181
Prateek Singh <92904643+PrateekSinghRajput@users.noreply.github.com>
2025-10-24 14:10:03 +0530

    Update Code.ino

--- Arduino/Flex_Sensor_Code/Code/Code.ino
+++ Arduino/Flex_Sensor_Code/Code/Code.ino
@@ -3,107 +3,98 @@
 //https://www.youtube.com/c/JustDoElectronics/videos
 
 #include <LiquidCrystal_I2C.h>
+#include <SoftwareSerial.h>
+#include <DFRobotDFPlayerMini.h>
+
 LiquidCrystal_I2C lcd(0x27, 16, 2);
-
-#include "SoftwareSerial.h"
-#include "DFRobotDFPlayerMini.h"
-DFRobotDFPlayerMini myDFPlayer;
-void printDetail(uint8_t type, int value);
-
 SoftwareSerial mySoftwareSerial(2, 3);
+DFRobotDFPlayerMini myDFPlayer;
 
 void setup() {
-
   lcd.init();
   lcd.backlight();
-  lcd.setCursor(0, 0);
-  lcd.print("Sign Language To");
-  lcd.setCursor(0, 1);
-  lcd.print("Speech Convert");
-  delay(4000);
-  lcd.clear();
 
-  pinMode(A0, INPUT);
-  pinMode(A1, INPUT);
-  pinMode(A2, INPUT);
+  lcd.setCursor(0, 0);
+  lcd.print("Sign Language");
+  lcd.setCursor(0, 1);
+  lcd.print("To Speech");
+  delay(3000);
+  lcd.clear();
 
   Serial.begin(9600);
   mySoftwareSerial.begin(9600);
-  Serial.begin(115200);
-  Serial.println();
-  Serial.println(F("Initializing DFPlayer..."));
 
+  Serial.println("Initializing DFPlayer...");
   if (!myDFPlayer.begin(mySoftwareSerial)) {
-    Serial.println(F("Unable to begin:"));
-    Serial.println(F("1.Please recheck the connection!"));
-    Serial.println(F("2.Please insert the SD card!"));
+    Serial.println("Unable to begin DFPlayer");
     while (true)
       ;
   }
-  Serial.println(F("DFPlayer Mini online."));
+  Serial.println("DFPlayer initialized");
   myDFPlayer.volume(30);
+
+  // Set analog inputs
+  pinMode(A0, INPUT);
+  pinMode(A1, INPUT);
+  pinMode(A2, INPUT);
+  pinMode(A3, INPUT);
 }
 
 void loop() {
+  // Read sensor values
+  int sensor1 = analogRead(A0);
+  int sensor2 = analogRead(A1);
+  int sensor3 = analogRead(A2);
+  int sensor4 = analogRead(A3);
 
-  int one = analogRead(A0);
-  int two = analogRead(A1);
-  int three = analogRead(A2);
+  Serial.print("Sensor1: ");
+  Serial.print(sensor1);
+  Serial.print("\tSensor2: ");
+  Serial.print(sensor2);
+  Serial.print("\tSensor3: ");
+  Serial.println(sensor3);
+  Serial.print("\tSensor4: ");
+  Serial.println(sensor4);
 
-  Serial.print("Sensor1 ");
-  Serial.print(one);
-  Serial.println("\t");
-
-  //One
-  if (one <= 150) {
+  if (sensor1 >= 772) {
     lcd.clear();
     lcd.setCursor(0, 0);
     lcd.print("I Am Hungry");
-    delay(500);
     myDFPlayer.play(1);
-    delay(1000);
+    delay(3000);
   }
 
-  if (one >= 300) {
+  if (sensor2 >= 730) {
     lcd.clear();
     lcd.setCursor(0, 0);
-    lcd.print("Help Me To");
-    lcd.setCursor(0, 1);
-    lcd.print("Reach Home");
-    delay(500);
-    myDFPlayer.play(2);
-    delay(1000);
-
+    lcd.print("Help Me To Rich Home");
+    myDFPlayer.play(3);
+    delay(3000);
   }
 
-  //Two
-  else if (two <= 240) {
+  if (sensor3 >= 720) {
     lcd.clear();
     lcd.setCursor(0, 0);
     lcd.print("Call The Police");
-
-    delay(500);
-    myDFPlayer.play(3);
-    delay(1000);
+    myDFPlayer.play(5);
+    delay(3000);
   }
 
-  //Three
-  else if (three <= 215) {
+
+  if (sensor4 >= 738) {
     lcd.clear();
     lcd.setCursor(0, 0);
-    lcd.print("I Need Water");
-    lcd.setCursor(1, 1);
-    lcd.print("");
-    delay(500);
-    myDFPlayer.play(5);
-    delay(1000);
-
+    lcd.print("I Need Help");
+    myDFPlayer.play(7);
+    delay(3000);
   }
 
   else {
     lcd.clear();
-    lcd.setCursor(1, 0);
-    lcd.print("NOTHING");
+    lcd.setCursor(0, 0);
+    lcd.print("Nothing Detected");
+    delay(1000);
   }
+
   delay(50);
 }
